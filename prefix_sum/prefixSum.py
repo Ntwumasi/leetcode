e24c924750d2972bacc62e8f002c7205d6f670b3
Nokio <63355641+Ntwumasi@users.noreply.github.com>
2025-05-07 10:51:55 -0400

    added two pointer examples

--- prefix_sum/prefixSum.py
+++ prefix_sum/prefixSum.py
@@ -1,7 +1,7 @@
 def answer_queries(nums, queries, limit):
-    prefix = [nums[0]]
-    for i in range(1, len(nums)):
-        prefix.append(nums[i] + prefix[-1])
+    prefix = [nums[0]] # adding the first element to the prefix sum
+    for i in range(1, len(nums)): # looping through the rest of the elements
+        prefix.append(nums[i] + prefix[-1]) # adding the current element to the last element of the prefix sum
     
     ans = []
     for x, y in queries:
