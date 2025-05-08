45caf8751fd5b67edb3b100ecc5dd1684c4ba899
Nokio <63355641+Ntwumasi@users.noreply.github.com>
2025-05-08 10:09:12 -0400

    day 3 leetcode

--- two_pointer/reverseString.py
+++
@@ -1,5 +0,0 @@
-def reverse_a_string(str, s):
-    s.reverse()
-    return s
-# Test the function
-print(reverse_a_string("hello", list("hello")))
--- two_pointer/squareAndSort.py
+++
@@ -1,5 +0,0 @@
-from typing import List
-
-class Solution:
-    def sortedSquares(self, nums: List[int]) -> List[int]:
-        return sorted(num ** 2 for num in nums)
